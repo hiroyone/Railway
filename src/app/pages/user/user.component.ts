8361419546fc292d2f4f0e1d6b64cdc67aee4d01
Elene Giunashvili <elegiunashvili232@gmail.com>
2024-06-11 19:33:22 +0400

    fixed

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
 })
-export class UserComponent {}
+export class UserComponent {
+  submitted = false;
+
+  constructor(private router: Router) {}
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log('Form submitted:', form.value);
+      this.submitted = true;
+      this.router.navigate(['/pay']); // Navigate to the pay page
+    }
+  }
+}
