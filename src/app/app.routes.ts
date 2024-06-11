295b619377b6f215ec893cd01bd74d1135c2fd16
Hiroki Yonetani <hiroki.yonetani@zalando.de>
2024-06-11 18:01:25 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { TicketsPageComponent } from './pages/tickets-page/tickets-page.component';
 import { InformationPageComponent } from './pages/information-page/information-page.component';
 import { UserComponent } from './pages/user/user.component';
+import { PayComponent } from './pages/pay/pay.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'tickets', component: TicketsPageComponent },
   { path: 'information', component: InformationPageComponent },
   { path: 'user', component: UserComponent },
+  { path: 'pay', component: PayComponent },
 ];
